9f2acc0d1d93264835e368cc198a5c7e87a3f1ae
NS <evilnood@gmail.com>
2021-10-25 21:44:07 +0300

    Добавьте файлы проекта.

---
+++ Multithreading.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+std::mutex threadLock;
+
+int counter = 0;
+
+void Function()
+{
+    threadLock.lock();
+
+    counter++;
+
+    threadLock.unlock();
+}
+
+int main()
+{
+    std::thread thread1(Function);
+    std::thread thread2(Function);
+    std::thread thread3(Function);
+
+    std::cout << counter<<std::endl;
+
+    thread1.join();
+    thread2.join();    
+    thread3.join();
+
+    std::cout << counter;
+
+    return 0;
+}
