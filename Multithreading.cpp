a2c80f4ff4dfb389e9584e21c4543b9f35904ffc
NS <evilnood@gmail.com>
2021-10-26 08:52:12 +0300

    Added unique_lock with example

--- Multithreading.cpp
+++ Multithreading.cpp
@@ -4,31 +4,52 @@
 #include <mutex>
 
 std::mutex threadLock;
+std::mutex unicLockMutex;
 
-int counter = 0;
+int firstcounter = 0;
+int secondcounter = 0;
 
-void Function()
+void MutexFunction()
 {
     threadLock.lock();
 
-    counter++;
+    firstcounter++;
 
     threadLock.unlock();
 }
 
+void UniqueLockMutexFunction()
+{
+    std::unique_lock<std::mutex> unicLock(unicLockMutex);
+    for (int i = 0; i < 10; i++)
+    {
+    secondcounter++;
+    }
+}
+
 int main()
 {
-    std::thread thread1(Function);
-    std::thread thread2(Function);
-    std::thread thread3(Function);
+    std::thread thread1(MutexFunction);
+    std::thread thread2(MutexFunction);
+    std::thread thread3(MutexFunction);
 
-    std::cout << counter<<std::endl;
+    std::cout << firstcounter<<std::endl;
 
     thread1.join();
     thread2.join();    
     thread3.join();
 
-    std::cout << counter;
+    std::cout << firstcounter<<std::endl;
+
+    std::thread unicthread1(UniqueLockMutexFunction);
+    std::thread unicthread2(UniqueLockMutexFunction);
+
+    std::cout << secondcounter<<std::endl;
+
+    unicthread1.join();
+    unicthread2.join();
+
+    std::cout << secondcounter << std::endl;
 
     return 0;
 }
